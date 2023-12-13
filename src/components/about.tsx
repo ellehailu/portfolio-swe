ee815ed5f968a6e876ae2f7aa232c67fef1e6256
ellehailu <hailu.elle@gmail.com>
2023-12-12 22:15:05 -0800

    update components

---
+++ src/components/about.tsx
@@ -0,0 +1,48 @@
+// Add bio page 
+
+const About = () => {
+    return (
+        <div>
+            <>
+            <h2>About me</h2>
+                <p>
+                    Welcome, I am glad you are here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem laboriosam nisi laudantium ratione molestiae temporibus ex odit corrupti repudiandae aliquid maxime recusandae consequuntur aperiam quis, quaerat minima quibusdam enim! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, modi. Sint nostrum ad accusantium dolore doloremque, omnis a odit tenetur voluptate eos, quas pariatur exercitationem porro molestiae. Odit, ipsum dolorem!
+                </p>
+            </>
+            <>
+                <image>
+                    <img src="https://www.theodysseyonline.com/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8xMDQ0MDA5MS9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTcyMTEzMjIyNX0.Et1pzAOJH9RgmQgFHxEQkPoqliT6myZBt98kAkQK33w/image.jpg?width=210" alt="Elshadai standing in a grass field looking at the camera" />
+                </image>
+            </>
+            <>
+                <h2>
+                    Experience
+                </h2>
+                <ul>
+                    <h3>Fullstack Software Engineer Intern</h3>
+                    All Star Directories | October 2023 - December 2023
+                    <h3>Technical Content Writer</h3>
+                    The Odigo Group | August 2021 - April 2023
+                    <h3>Research and Remediation Associate</h3>
+                    Wells Fargo Bank | September 2020 - August 2021
+                    <h3>Americorps VISTA Summer Associate</h3>
+                    Good Cheer Food Bank | June 2020 - September 2020
+                </ul>
+            </>
+            <>
+            <h2>
+                Education
+            </h2>
+                <ul>
+                Epicodus, Portland OR <br />
+                <span>Web and Mobile Development, 2023</span><br />
+                
+                <br />George Fox University, Newberg OR <br />
+                <span>Bachelor of Science Cognitive Science: Human and Artificial Intelligence, 2016 - 2020</span>
+                </ul>
+            </>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/contact.tsx
@@ -0,0 +1,13 @@
+
+const Contact = () => {
+    return (
+        <>
+            <h1>
+                Get in touch! I'd love to connect!
+            </h1>
+            <h2>Email: hailu.elle@gmail.com</h2>
+        </>  
+    )
+}
+
+export default Contact
